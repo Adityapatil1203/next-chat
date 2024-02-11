9c3d6eb10a9a0a71712aa2309d5a66273d6472d9
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-02-11 12:42:11 +0530

    .

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "CHAT",
+  description: "CHAT",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Toaster position="bottom-center " />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/Footer.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().toLocaleString("default", { month: "long" });
+  return (
+    <footer className="p-4 footer footer-center bg-base-300 text-base-content">
+      <aside>
+        <p>
+          {currentMonth} {currentYear} - Copyright © |
+          <Link href={"https://tusharupadhyay.vercel.app/"}>
+            {" "}
+            <span className="transition-all duration-200 hover:text-white">
+              Tushar Upadhyay
+            </span>
+          </Link>
+        </p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
